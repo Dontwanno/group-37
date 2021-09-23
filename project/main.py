186113f6670f266932686ad26d752cc1c267e595
tgrooff <twan.grooff@xs4all.nl>
2021-09-23 10:43:39 +0200

    Added user input story

--- project/main.py
+++ project/main.py
@@ -1,9 +1,10 @@
+from console_user_interface import request_user, request_calendar
 from project.displaySchedule import displaySchedule
-
 
 def main():
     # Request user to enter tasks:
-
+    request_user()
+    request_calendar()
     # Store list of tasks:
 
     # Extract list of tasks:
@@ -11,9 +12,10 @@ def main():
     # Display list of tasks:
     #       List of tasks format (for now): [startTime][endTime][name]
     #                                   eg: [1500][1600][meeting]
-    temporary_list = [[845, 1045, 1345, 1545], [1030, 1230, 1530, 1800], ['Walk my dog', 'Meeting with SEM boys', 'Do my homework', 'Crying session']]
+    temporary_list = [[845, 1045, 1345, 1545], [1030, 1230, 1530, 1800],
+                      ['Walk my dog', 'Meeting with SEM boys', 'Do my homework', 'Crying session']]
     displaySchedule(temporary_list)
 
 
 if __name__ == '__main__':
-        main()
+    main()
