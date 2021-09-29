ebc3f0ef7290a68d70df9010c7eaec8a7399ae6e
Pelle Wiersma <pelle.wiersma@gmail.com>
2021-09-29 18:31:08 +0200

    Added fit task function

--- project/displaySchedule.py
+++ project/displaySchedule.py
@@ -1,15 +1,15 @@
-def formatIntToTimeString(timeInt):
-    hour = str(timeInt)[0: len(str(timeInt)) - 2]
-    min = str(timeInt)[len(str(timeInt)) - 2: len(str(timeInt))]
+def format_int_to_time_string(time_int):
+    hour = str(time_int)[0: len(str(time_int)) - 2]
+    min = str(time_int)[len(str(time_int)) - 2: len(str(time_int))]
     return hour + ':' + min
 
 
-def displaySchedule(task_list):
+def display_schedule(task_list):
     temp_task_list = task_list.get_task_list()
     print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
     print('SCHEDULE:')
     for i in range(len(temp_task_list)):
-        print('- ' + formatIntToTimeString(temp_task_list[i].get_start_time()) + ' --> ' +
-              formatIntToTimeString(temp_task_list[i].get_end_time()) + ': ' + temp_task_list[i].get_description())
+        print('- ' + format_int_to_time_string(temp_task_list[i].get_start_time()) + ' --> ' +
+              format_int_to_time_string(temp_task_list[i].get_end_time()) + ': ' + temp_task_list[i].get_description())
     print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
     del temp_task_list
--- project/user_input.py
+++ project/user_input.py
@@ -9,19 +9,31 @@ def new_task(task_list):
     """
     print("Entering new task:")
     print('--------------------------------------')
+
     try:
-        description = input("Description: ")
-        start_time = int(input("Start time: "))
-        end_time = int(input("End time: "))
-        if start_time > 2400 or end_time > 2400:
-            raise ValueError
-        # Add task to the task list
-        task_list.add_task(Task(start_time, end_time, description, False))
+        print('Would you like to enter the start and end time for your task?')
+        answer = None
+        while answer not in ("yes", "no"):
+            # repeat when user doesn't enter yes or no
+            answer = input("Enter yes or no: ")
+            if answer == "yes":
+                description = input("Description: ")
+                start_time = int(input("Start time: "))
+                end_time = int(input("End time: "))
+                if start_time > 2400 or end_time > 2400:
+                    raise ValueError
+                # Add task to the task list
+                task_list.add_task(Task(start_time, end_time, description, False))
+            elif answer == "no":
+                description = input("Description: ")
+                task_length = int(input("Task length: "))
+                # Fit new task
+                task_list.fit_task(Task(task_length, 0, description, False))
     except ValueError:
         # if input was not finished, give message and return old task list
         print("Time must be valid and in integer format e.g. 8:45 = 845, 20:15 = 2015.")
         print("Try again.")
         return
     # if everything went smooth, return
-    print("Task stored successfully")
+    # print("Task stored successfully")
     return
