31fd68134452633f282a46d0524278a66c7c1ae5
Pelle Wiersma <pelle.wiersma@gmail.com>
2021-10-04 17:21:19 +0200

    Refactor code

--- project/apis.py
+++ project/apis.py
@@ -16,22 +16,22 @@ from classes import Task, TaskList
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 
-def get_google_calander(task_list):
+def get_google_calendar(task_list):
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists('APIs/token.json'):
+        creds = Credentials.from_authorized_user_file('APIs/token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('APIs/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open('APIs/token.json', 'w') as token:
             token.write(creds.to_json())
 
     service = build('calendar', 'v3', credentials=creds)
@@ -56,6 +56,6 @@ def get_google_calander(task_list):
         description = event['summary']
         task_list.add_task(Task(start_time, end_time, description, True))
 
-        print("Added " + event['summary'] + " to your WorkValve calander")
+        print("Added " + event['summary'] + " to your WorkValve calendar")
 
     return task_list
---
+++ project/ui.py
@@ -0,0 +1,76 @@
+from classes import Task, TaskList
+
+
+def new_task(task_list, start_end):
+    """
+    Allows user to add a new task
+    :param task_list: List of existing tasks, class TaskList
+    :return: nothing. TaskList object got updated
+    """
+    print("Entering new task:")
+    print('--------------------------------------')
+
+    try:
+        print('Would you like to enter the start and end time for your task?')
+        answer = None
+        while answer not in ("yes", "no"):
+            # repeat when user doesn't enter yes or no
+            answer = input("Enter yes or no: ")
+            if answer == "yes":
+                description = input("Description: ")
+                start_time = int(input("Start time: "))
+                end_time = int(input("End time: "))
+                if start_time > 2400 or end_time > 2400:
+                    raise ValueError
+                # Add task to the task list
+                task_list.add_task(Task(start_time, end_time, description, False))
+            elif answer == "no":
+                description = input("Description: ")
+                task_length = int(input("Task length: "))
+                # Fit new task
+                task_list.fit_task(Task(task_length, 0, description, False), start_end)
+    except ValueError:
+        # if input was not finished, give message and return old task list
+        print("Time must be valid and in integer format e.g. 8:45 = 845, 20:15 = 2015.")
+        print("Try again.")
+        return
+    # if everything went smooth, return
+    # print("Task stored successfully")
+    return
+
+
+def set_start_end_time(start_end):
+    print("Please enter the start and end time of your workday.\n")
+    start_time = input("Start time: ")
+    end_time = input("End time: ")
+    print(f"\nThe start time you entered is: {start_time[:2]}:{start_time[2:]}\nThe end time you entered is:"
+          f" {end_time[:2]}:{end_time[2:]}\nAre these times correct?")
+    start_end[0] = int(start_time)
+    start_end[1] = int(end_time)
+    answer = None
+    while answer not in ("yes", "no"):
+        # repeat when user doesn't enter yes or no
+        answer = input("Enter yes or no: ")
+        if answer == "yes":
+            return
+        elif answer == "no":
+            set_start_end_time(start_end)
+        else:
+            print("Please enter yes or no.")
+
+
+def format_int_to_time_string(time_int):
+    hour = str(time_int)[0: len(str(time_int)) - 2]
+    min = str(time_int)[len(str(time_int)) - 2: len(str(time_int))]
+    return hour + ':' + min
+
+
+def display_schedule(task_list):
+    temp_task_list = task_list.get_task_list()
+    print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+    print('SCHEDULE:')
+    for i in range(len(temp_task_list)):
+        print('- ' + format_int_to_time_string(temp_task_list[i].get_start_time()) + ' --> ' +
+              format_int_to_time_string(temp_task_list[i].get_end_time()) + ': ' + temp_task_list[i].get_description())
+    print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+    del temp_task_list
