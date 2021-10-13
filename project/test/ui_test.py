992dfd848385a179f69d7e86ab13bf1d69131830
Simon Cajagi <simiken1234@gmail.com>
2021-10-13 15:13:04 +0200

    Tested format_int_to_time_string

---
+++ project/test/ui_test.py
@@ -0,0 +1,7 @@
+from project.ui import format_int_to_time_string
+
+
+def test_format_int_to_time_string():
+    assert format_int_to_time_string(0) == "0:00"
+    assert format_int_to_time_string(100) == "1:00"
+    assert format_int_to_time_string(1130) == "11:30"
